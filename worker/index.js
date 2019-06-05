c16d20913ba35daaa6203c9e5c975bb9f7ecd78b
rogerwschmidt <rogerschmidt@gmail.com>
2019-06-05 01:30:00 -0700

    docker-compose setup

--- worker/index.js
+++ worker/index.js
@@ -14,6 +14,7 @@ function fib(index) {
 }
 
 sub.on('message', (channel, message) => {
+  console.log('worker', message)
   redisClient.hset('values', message, fib(parseInt(message)));
 });
 sub.subscribe('insert');
